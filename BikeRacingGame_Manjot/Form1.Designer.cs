b76ba0cf6df36e3d7bf80b456777980246d141b7
Manjots7847@gmail.com <Manjots7847@gmail.com>
2021-06-08 22:24:55 +0300

    Add project files.

---
+++ BikeRacingGame_Manjot/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace BikeRacingGame_Manjot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MinimumBet = new System.Windows.Forms.Label();
+            this.labelAlsBet = new System.Windows.Forms.Label();
+            this.labelBobsBet = new System.Windows.Forms.Label();
+            this.labelJoesBet = new System.Windows.Forms.Label();
+            this.numericUpDownCar = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownBets = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonAI = new System.Windows.Forms.RadioButton();
+            this.radioButtonBob = new System.Windows.Forms.RadioButton();
+            this.radioButtonJoe = new System.Windows.Forms.RadioButton();
+            this.buttonRace = new System.Windows.Forms.Button();
+            this.buttonBets = new System.Windows.Forms.Button();
+            this.Racer4 = new System.Windows.Forms.PictureBox();
+            this.Racer3 = new System.Windows.Forms.PictureBox();
+            this.Racer2 = new System.Windows.Forms.PictureBox();
+            this.Racer1 = new System.Windows.Forms.PictureBox();
+            this.RaceTrack = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RaceTrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MinimumBet);
+            this.groupBox1.Controls.Add(this.labelAlsBet);
+            this.groupBox1.Controls.Add(this.labelBobsBet);
+            this.groupBox1.Controls.Add(this.labelJoesBet);
+            this.groupBox1.Controls.Add(this.numericUpDownCar);
+            this.groupBox1.Controls.Add(this.numericUpDownBets);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButtonAI);
+            this.groupBox1.Controls.Add(this.radioButtonBob);
+            this.groupBox1.Controls.Add(this.radioButtonJoe);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 78);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(1048, 158);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Betting System";
+            // 
+            // MinimumBet
+            // 
+            this.MinimumBet.AutoSize = true;
+            this.MinimumBet.Location = new System.Drawing.Point(324, 47);
+            this.MinimumBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MinimumBet.Name = "MinimumBet";
+            this.MinimumBet.Size = new System.Drawing.Size(99, 17);
+            this.MinimumBet.TabIndex = 26;
+            this.MinimumBet.Text = "Minimum Bet";
+            // 
+            // labelAlsBet
+            // 
+            this.labelAlsBet.AutoSize = true;
+            this.labelAlsBet.BackColor = System.Drawing.Color.White;
+            this.labelAlsBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAlsBet.Location = new System.Drawing.Point(705, 130);
+            this.labelAlsBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelAlsBet.Name = "labelAlsBet";
+            this.labelAlsBet.Size = new System.Drawing.Size(154, 19);
+            this.labelAlsBet.TabIndex = 25;
+            this.labelAlsBet.Text = "AI hasn\'t placed bet";
+            // 
+            // labelBobsBet
+            // 
+            this.labelBobsBet.AutoSize = true;
+            this.labelBobsBet.BackColor = System.Drawing.Color.White;
+            this.labelBobsBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelBobsBet.Location = new System.Drawing.Point(705, 81);
+            this.labelBobsBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBobsBet.Name = "labelBobsBet";
+            this.labelBobsBet.Size = new System.Drawing.Size(168, 19);
+            this.labelBobsBet.TabIndex = 24;
+            this.labelBobsBet.Text = "Bob hasn\'t placed bet";
+            // 
+            // labelJoesBet
+            // 
+            this.labelJoesBet.AutoSize = true;
+            this.labelJoesBet.BackColor = System.Drawing.Color.White;
+            this.labelJoesBet.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelJoesBet.Location = new System.Drawing.Point(705, 34);
+            this.labelJoesBet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelJoesBet.Name = "labelJoesBet";
+            this.labelJoesBet.Size = new System.Drawing.Size(188, 19);
+            this.labelJoesBet.TabIndex = 23;
+            this.labelJoesBet.Text = "Manjot hasn\'t placed bet";
+            // 
+            // numericUpDownCar
+            // 
+            this.numericUpDownCar.Location = new System.Drawing.Point(549, 91);
+            this.numericUpDownCar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownCar.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDownCar.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCar.Name = "numericUpDownCar";
+            this.numericUpDownCar.Size = new System.Drawing.Size(59, 23);
+            this.numericUpDownCar.TabIndex = 22;
+            this.numericUpDownCar.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownBets
+            // 
+            this.numericUpDownBets.Location = new System.Drawing.Point(549, 44);
+            this.numericUpDownBets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericUpDownBets.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownBets.Name = "numericUpDownBets";
+            this.numericUpDownBets.Size = new System.Drawing.Size(59, 23);
+            this.numericUpDownBets.TabIndex = 21;
+            this.numericUpDownBets.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(516, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = " #";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "$";
+            // 
+            // radioButtonAI
+            // 
+            this.radioButtonAI.AutoSize = true;
+            this.radioButtonAI.Location = new System.Drawing.Point(45, 130);
+            this.radioButtonAI.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonAI.Name = "radioButtonAI";
+            this.radioButtonAI.Size = new System.Drawing.Size(43, 21);
+            this.radioButtonAI.TabIndex = 2;
+            this.radioButtonAI.TabStop = true;
+            this.radioButtonAI.Text = "AI";
+            this.radioButtonAI.UseVisualStyleBackColor = true;
+            this.radioButtonAI.CheckedChanged += new System.EventHandler(this.radioButtonAI_CheckedChanged);
+            // 
+            // radioButtonBob
+            // 
+            this.radioButtonBob.AutoSize = true;
+            this.radioButtonBob.Location = new System.Drawing.Point(45, 79);
+            this.radioButtonBob.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonBob.Name = "radioButtonBob";
+            this.radioButtonBob.Size = new System.Drawing.Size(57, 21);
+            this.radioButtonBob.TabIndex = 1;
+            this.radioButtonBob.TabStop = true;
+            this.radioButtonBob.Text = "Bob";
+            this.radioButtonBob.UseVisualStyleBackColor = true;
+            this.radioButtonBob.CheckedChanged += new System.EventHandler(this.radioButtonBob_CheckedChanged);
+            // 
+            // radioButtonJoe
+            // 
+            this.radioButtonJoe.AutoSize = true;
+            this.radioButtonJoe.Location = new System.Drawing.Point(45, 34);
+            this.radioButtonJoe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButtonJoe.Name = "radioButtonJoe";
+            this.radioButtonJoe.Size = new System.Drawing.Size(77, 21);
+            this.radioButtonJoe.TabIndex = 0;
+            this.radioButtonJoe.TabStop = true;
+            this.radioButtonJoe.Text = "Manjot";
+            this.radioButtonJoe.UseVisualStyleBackColor = true;
+            this.radioButtonJoe.CheckedChanged += new System.EventHandler(this.radioButtonJoe_CheckedChanged);
+            // 
+            // buttonRace
+            // 
+            this.buttonRace.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRace.Location = new System.Drawing.Point(805, 27);
+            this.buttonRace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonRace.Name = "buttonRace";
+            this.buttonRace.Size = new System.Drawing.Size(123, 39);
+            this.buttonRace.TabIndex = 34;
+            this.buttonRace.Text = "Race!";
+            this.buttonRace.UseVisualStyleBackColor = true;
+            this.buttonRace.Click += new System.EventHandler(this.buttonRace_Click);
+            // 
+            // buttonBets
+            // 
+            this.buttonBets.Location = new System.Drawing.Point(169, 15);
+            this.buttonBets.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonBets.Name = "buttonBets";
+            this.buttonBets.Size = new System.Drawing.Size(149, 52);
+            this.buttonBets.TabIndex = 33;
+            this.buttonBets.Text = "Place Bet";
+            this.buttonBets.UseVisualStyleBackColor = true;
+            this.buttonBets.Click += new System.EventHandler(this.buttonBets_Click);
+            // 
+            // Racer4
+            // 
+            this.Racer4.BackColor = System.Drawing.Color.White;
+            this.Racer4.Image = global::BikeRacingGame_Manjot.Properties.Resources.bike44;
+            this.Racer4.Location = new System.Drawing.Point(17, 596);
+            this.Racer4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Racer4.Name = "Racer4";
+            this.Racer4.Size = new System.Drawing.Size(133, 62);
+            this.Racer4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Racer4.TabIndex = 39;
+            this.Racer4.TabStop = false;
+            // 
+            // Racer3
+            // 
+            this.Racer3.BackColor = System.Drawing.Color.White;
+            this.Racer3.Image = global::BikeRacingGame_Manjot.Properties.Resources.bike33;
+            this.Racer3.Location = new System.Drawing.Point(17, 510);
+            this.Racer3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Racer3.Name = "Racer3";
+            this.Racer3.Size = new System.Drawing.Size(133, 62);
+            this.Racer3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Racer3.TabIndex = 38;
+            this.Racer3.TabStop = false;
+            // 
+            // Racer2
+            // 
+            this.Racer2.BackColor = System.Drawing.Color.White;
+            this.Racer2.Image = global::BikeRacingGame_Manjot.Properties.Resources.bike22;
+            this.Racer2.Location = new System.Drawing.Point(17, 416);
+            this.Racer2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Racer2.Name = "Racer2";
+            this.Racer2.Size = new System.Drawing.Size(133, 62);
+            this.Racer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Racer2.TabIndex = 37;
+            this.Racer2.TabStop = false;
+            // 
+            // Racer1
+            // 
+            this.Racer1.BackColor = System.Drawing.Color.White;
+            this.Racer1.Image = global::BikeRacingGame_Manjot.Properties.Resources.bike11;
+            this.Racer1.Location = new System.Drawing.Point(17, 321);
+            this.Racer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Racer1.Name = "Racer1";
+            this.Racer1.Size = new System.Drawing.Size(133, 62);
+            this.Racer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Racer1.TabIndex = 36;
+            this.Racer1.TabStop = false;
+            // 
+            // RaceTrack
+            // 
+            this.RaceTrack.Image = global::BikeRacingGame_Manjot.Properties.Resources.track111;
+            this.RaceTrack.Location = new System.Drawing.Point(-1, 242);
+            this.RaceTrack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RaceTrack.Name = "RaceTrack";
+            this.RaceTrack.Size = new System.Drawing.Size(1067, 492);
+            this.RaceTrack.TabIndex = 27;
+            this.RaceTrack.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 737);
+            this.Controls.Add(this.Racer4);
+            this.Controls.Add(this.Racer3);
+            this.Controls.Add(this.Racer2);
+            this.Controls.Add(this.Racer1);
+            this.Controls.Add(this.RaceTrack);
+            this.Controls.Add(this.buttonRace);
+            this.Controls.Add(this.buttonBets);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Bike Race Developed By Manjot";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Racer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RaceTrack)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label MinimumBet;
+        private System.Windows.Forms.Label labelAlsBet;
+        private System.Windows.Forms.Label labelBobsBet;
+        private System.Windows.Forms.Label labelJoesBet;
+        private System.Windows.Forms.NumericUpDown numericUpDownCar;
+        private System.Windows.Forms.NumericUpDown numericUpDownBets;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonAI;
+        private System.Windows.Forms.RadioButton radioButtonBob;
+        private System.Windows.Forms.RadioButton radioButtonJoe;
+        private System.Windows.Forms.Button buttonRace;
+        private System.Windows.Forms.Button buttonBets;
+        private System.Windows.Forms.PictureBox RaceTrack;
+        private System.Windows.Forms.PictureBox Racer1;
+        private System.Windows.Forms.PictureBox Racer2;
+        private System.Windows.Forms.PictureBox Racer3;
+        private System.Windows.Forms.PictureBox Racer4;
+    }
+}
+
